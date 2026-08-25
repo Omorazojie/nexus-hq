16d84bac3228593b1ba251ff52114813c28ce0e9
Omorazojie <allinfonext@gmail.com>
2026-08-25 20:52:31 +0100

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -18,3 +18,24 @@ export type Company = {
   mission: string;
   created_at: string;
 };
+
+export type Position = {
+  id: string;
+  company_id: string;
+  title: string;
+  position_type: string;
+  location: string;
+  pay: string;
+  description: string;
+  expectations: string;
+  created_at: string;
+};
+
+export type Application = {
+  id: string;
+  position_id: string;
+  applicant_name: string;
+  applicant_email: string;
+  message: string;
+  created_at: string;
+};
